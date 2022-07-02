82facf46d54d113ae5b7147028f5111a81e4644f
Ashley Mays <ashleymays091@gmail.com>
2022-07-02 16:06:21 -0700

    Add text, change colors, and organize files

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let form = document.getElementById("piano");
+let notes = [];
+let noteName = "";
+let chords = [];
+let inputs = document.getElementsByTagName("input");
+let chordsList;
+
+// clear list of chords in document
+function clearChordsList() {
+    chordsList = document.getElementById("chords-list");
+    while (chordsList.firstChild) {
+        chordsList.removeChild(chordsList.firstChild);
+    }
+}
+
+// remove 'M' for major chords for better readability
+function removeMajor() {
+    let index = chords[0].indexOf('M');
+    if (index != -1) {
+        chords[0] = chords[0].slice(0, index) + chords[0].slice(index + 1);
+    }
+}
+
+// get list of notes from form
+function getChosenNotes() {
+    for (let i in inputs) {
+        if (inputs[i].checked) {
+            noteName = inputs[i].value;
+            notes.push(noteName);
+        }
+    }
+}
+
+function submitForm() {
+    clearChordsList();
+    getChosenNotes();
+    chords = Tonal.Chord.detect(notes); // get chords from the list of notes
+    notes.length = 0;   // reset notes array so that user can input more
+
+    // show chords in document
+    chordElement = document.createElement("h1");
+    chordElement.setAttribute("class", "chord-name");
+    removeMajor();
+    chordElement.innerHTML = chords[0];
+    chordsList.appendChild(chordElement); // add element to DOM
+}
+
+function clearForm() {
+    clearChordsList();
+    for (let i in inputs) {
+        inputs[i].checked = false;
+    }
+}
